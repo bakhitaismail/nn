9c1f937ce3f9bd250632a932e5c183a1a959dc13
bakhitaismail <bakhitaismailk@gmail.com>
2022-02-20 22:50:40 +0300

    first commit

---
+++ src/assignment.kt
@@ -0,0 +1,21 @@
+fun main(){
+   people("Bakhita")
+    modules(243, 575)
+    add(45, 56,67)
+    interesting()
+}
+fun people(name:String){
+    println("Hello Bakhita")
+}
+fun modules(a:Int, b:Int){
+    var modules=a%b
+    println(modules)
+
+}
+fun add(c:Int, d:Int, e:Int){
+    var add=(c+d+e)
+    println(add)
+}
+fun interesting(){
+    println("I can laugh out loud")
+}
